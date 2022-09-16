1825d5c48a7de0a6238354c52ad1d02487c5574a
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-09-16 13:13:50 +0700

    Add sonar properties file

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  console.log("Test")
   return (
     <div className={styles.container}>
       <Head>
