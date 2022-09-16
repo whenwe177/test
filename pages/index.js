97309d833ab335cce985cc337f0a7cfa09727a35
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-09-16 21:00:23 +0700

    tt

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   console.log("Test");
   console.log("Test");
   console.log("Test");
+  console.log("Test");
   return (
     <div className={styles.container}>
       <Head>
