0a231f5e54f9d021c0b1250d586e5aa4fbbe8192
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-09-16 15:27:03 +0700

    Deploy after build

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  console.log("Test")
+  console.log("Test");
+  console.log("Test");
+  console.log("Test");
   return (
     <div className={styles.container}>
       <Head>
